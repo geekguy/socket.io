902e2f68c23116905602e1777d6f0eb78451fcb3
Dhaval Trivedi <dhaval@hackerrank.com>
2015-07-03 03:45:26 +0530

    adding proper raw data handler

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -10,18 +10,25 @@ var mp3Hash = {
 }
 
 // some code to make sure we can read aws json :/.
-// app.use(function(req, res, next) {
-//   req.rawBody = '';
-//   req.setEncoding('utf8');
+app.use(function(req, res, next) {
+  var contentType = req.headers['content-type'] || ''
+    , mime = contentType.split(';')[0];
 
-//   req.on('data', function(chunk) {
-//     req.rawBody += chunk;
-//   });
+  if (mime != 'text/plain') {
+    return next();
+  }
 
-//   req.on('end', function() {
-//     next();
-//   });
-// });
+  var data = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+  req.on('end', function() {
+    req.rawBody = data;
+    next();
+  });
+});
+
 app.use(express.bodyParser());
 
 app.use(express.logger());
