f663d8ba6a0f56cf5a1eaaa3bd3acd3a9018b991
AnirudhTiwari <anirudhtiwari91@gmail.com>
2015-07-03 07:43:20 +0530

    GitHub WebHook Complete 3

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -93,8 +93,8 @@ app.post('/aws-webhook', function(req, res) {
 app.post('/git-webhook', function(req, res){
 	if ((res.req.body.action=="opened" || res.req.body.action=="labeled" || res.req.body.action=="unlabeled" ) && res.req.body.issue.assignee!=null){
 		for (var i=0; i < res.req.body.issue.labels.length; i++){
-			if(res.req.body.issue.labels.name == "P0" || res.req.body.issue.labels.name == "Critical"){
-				var msg = "New issue Assigned to" + res.req.body.issue.assignee + "of " + res.req.body.issue.labels.name + "name"
+			if(res.req.body.issue.labels[i].name == "P0" || res.req.body.issue.labels[i].name == "Critical"){
+				var msg = "New issue Assigned to" + res.req.body.issue.assignee.login + "of " + res.req.body.issue.labels[i].name + "name"
 				io.sockets.emit('new_notification', msg);
 			}
 		}
